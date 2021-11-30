cdf442976e0725690707aa8a7995d303108b8177
Brian McMaster <brian@mcmpest.com>
2021-11-30 11:26:22 -0500

    [IMP] black, isort, prettier

--- product_contract_variable_quantity/models/product_template.py
+++ product_contract_variable_quantity/models/product_template.py
@@ -5,24 +5,24 @@ from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     qty_type = fields.Selection(
         selection=[
-            ('fixed', 'Fixed quantity'),
-            ('variable', 'Variable quantity'),
+            ("fixed", "Fixed quantity"),
+            ("variable", "Variable quantity"),
         ],
         required=False,
-        default='fixed',
+        default="fixed",
         string="Qty. type",
     )
     qty_formula_id = fields.Many2one(
         comodel_name="contract.line.qty.formula", string="Qty. formula"
     )
 
-    @api.onchange('is_contract')
+    @api.onchange("is_contract")
     def _change_is_contract(self):
-        """ Clear the relation to contract_template_id when downgrading
+        """Clear the relation to contract_template_id when downgrading
         product from contract
         """
         if not self.is_contract:
