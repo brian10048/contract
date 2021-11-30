a4e16b29e6f917aef740fa2a801e70f4d412204b
Brian McMaster <brian@mcmpest.com>
2021-11-30 11:32:00 -0500

    [MIG] product_contract_variable_quantity

--- product_contract_variable_quantity/__manifest__.py
+++ product_contract_variable_quantity/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Contract Variable Quantity",
     "summary": """
         Product contract with variable quantity""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/contract",
